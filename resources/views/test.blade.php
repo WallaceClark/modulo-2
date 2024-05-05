8f3b3dad28986391ce0f382b1923486538f95c2b
Wallace Clark <walclark@gmail.com>
2024-05-04 23:27:58 -0300

    View - Organizando a visualização

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
+<x-app-layout>
+  <x-slot name="header">
+    <h3>HEADER</h3>
+  </x-slot>
   <ol>
     @php
-      $user = Auth::user();
-      $client = App\Models\Client::where('user_id',$user->id)->first();
+      $user = auth()->user();
+      $client = $user->client;
       $signature = $client->signatures->first();
       $plan = $signature->plan;
       $status = $signature->status;
@@ -18,12 +13,6 @@
     <li>Nome: {{ $user->name }}</li>
     <li>Documento: {{ $client->document }}</li>
     <li>Plano: {{ $plan->name.' - '.$plan->short_description }}</li>
-    <li>Status da assinatura: {{ $status->name }}</li>
-    <hr>
-    <li>Nome: {{ auth()->user()->name }}</li>
-    <li>Documento: {{ auth()->user()->client->document }}</li>
-    <li>Plano: {{ auth()->user()->client->signatures->first()->plan->name.' - '.auth()->user()->client->signatures->first()->plan->short_description }}</li>
-    <li>Status da assinatura: {{ auth()->user()->client->signatures->first()->status->name }}</li>
+    <li>Status da assinatura: {{ $status->name }}</li>    
   </ol>
-</body>
-</html>
+</x-app-layout>
