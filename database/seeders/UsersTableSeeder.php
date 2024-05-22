e7397756040c73c71fdaccea628c9ec26d135d9b
Wallace Clark <walclark@gmail.com>
2024-05-22 16:52:38 -0300

    Factories

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,11 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,10 +13,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            'name'  => Str::random(10),
-            'email' => Str::random(10).'@mail.com',
-            'password'  => Hash::make('password'),
-        ]);
+        User::factory(10)
+            ->hasClient(1)
+            ->create();
     }
 }
