1f65dbe1feceb9645e964860b4962c7e720b2767
Wallace Clark <walclark@gmail.com>
2024-05-24 15:07:15 -0300

    Factory para planos e campos customizados no model Plan usando Attribute

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -21,5 +22,34 @@ class Plan extends Model
     public function signatures()
     {
         return $this->hasMany(Signature::class);
+    }
+
+    public function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => ucfirst($value)
+        );
+    }
+
+    public function cod(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => strtoupper($value),
+            set: fn($value) => strtolower($value)
+        );
+    }
+
+    public function fullnameOriginal(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attributes) => $attributes['cod'].' - '.$attributes['name']
+        );
+    }
+
+    public function fullname(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attributes) => strtoupper($attributes['cod']).' - '.ucfirst($attributes['name'])
+        );
     }
 }
